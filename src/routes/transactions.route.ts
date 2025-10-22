93aca2d31ed0ae4de248e39b8a7611372d737a89
reva <reva.erica24@gmail.com>
2025-10-22 14:10:24 +0700

    Add transaction routes with authentication middleware

--- src/routes/transactions.route.ts
+++ src/routes/transactions.route.ts
@@ -1,1 +1,19 @@
+import { Router } from "express";
+import { authMiddleware } from "../middlewares/auth.middleware";
+import {
+  createTransaction,
+  getTransactions,
+  getTransactionById,
+  getStatistics,
+} from "../controllers/transactions.controller";
 
+const router = Router();
+
+router.use(authMiddleware);
+
+router.post("/", createTransaction);
+router.get("/", getTransactions);
+router.get("/:id", getTransactionById);
+router.get("/statistics", getStatistics);
+
+export default router;
