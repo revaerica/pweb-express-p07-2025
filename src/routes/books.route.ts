e9dbd54349a48fa793a0758711106add42ff772d
reva <reva.erica24@gmail.com>
2025-10-22 14:09:34 +0700

    Add book routes with authentication middleware

--- src/routes/books.route.ts
+++ src/routes/books.route.ts
@@ -1,1 +1,23 @@
+import { Router } from "express";
+import { authMiddleware } from "../middlewares/auth.middleware";
+import {
+  createBook,
+  getBooks,
+  getBookById,
+  getBooksByGenre,
+  updateBook,
+  deleteBook,
+} from "../controllers/books.controller";
 
+const router = Router();
+
+router.use(authMiddleware);
+
+router.post("/", createBook);
+router.get("/", getBooks);
+router.get("/genre/:genre_id", getBooksByGenre);
+router.get("/:id", getBookById);
+router.patch("/:id", updateBook);
+router.delete("/:id", deleteBook);
+
+export default router;
